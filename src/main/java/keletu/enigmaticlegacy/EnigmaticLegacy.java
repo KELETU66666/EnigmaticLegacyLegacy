4158280747841ca6371b353e0e460acd2e181b8a
a <EYCHFSJD@gmail.com>
2024-07-10 20:41:27 +0800

    version bump

--- src/main/java/keletu/enigmaticlegacy/EnigmaticLegacy.java
+++ src/main/java/keletu/enigmaticlegacy/EnigmaticLegacy.java
@@ -40,13 +40,13 @@ import net.minecraftforge.oredict.OreDictionary;
         modid = EnigmaticLegacy.MODID,
         name = EnigmaticLegacy.MOD_NAME,
         version = EnigmaticLegacy.VERSION,
-        dependencies = "required-after:baubles"
+        dependencies = "required-after:baubles;after:patchouli"
 )
 public class EnigmaticLegacy {
 
     public static final String MODID = "enigmaticlegacy";
     public static final String MOD_NAME = "Enigmatic Legacy²";
-    public static final String VERSION = "0.0.3";
+    public static final String VERSION = "0.0.4";
 
     public static ItemArmor.ArmorMaterial ARMOR_ETHERIUM = EnumHelper.addArmorMaterial("etherium", EnigmaticLegacy.MODID + ":etherium", 132, new int[] { 4, 7, 9, 4 }, 24, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 4F);
     public static Item.ToolMaterial ETHERIUM = EnumHelper.addToolMaterial("etherium", 5, 3000, 8.0F, 5.0F, 32);
