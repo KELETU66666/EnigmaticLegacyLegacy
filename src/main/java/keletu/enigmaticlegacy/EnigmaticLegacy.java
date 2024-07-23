f5ac9a296a7e7a99546fc3de444ae0c12c26468d
a <EYCHFSJD@gmail.com>
2024-07-23 15:55:46 +0800

    version bump

--- src/main/java/keletu/enigmaticlegacy/EnigmaticLegacy.java
+++ src/main/java/keletu/enigmaticlegacy/EnigmaticLegacy.java
@@ -64,7 +64,7 @@ public class EnigmaticLegacy {
 
     public static final String MODID = "enigmaticlegacy";
     public static final String MOD_NAME = "Enigmatic Legacy²";
-    public static final String VERSION = "0.2.2";
+    public static final String VERSION = "0.2.3";
 
     @SidedProxy(clientSide = "keletu.enigmaticlegacy.proxy.ClientProxy", serverSide = "keletu.enigmaticlegacy.proxy.CommonProxy")
     public static CommonProxy proxy;
