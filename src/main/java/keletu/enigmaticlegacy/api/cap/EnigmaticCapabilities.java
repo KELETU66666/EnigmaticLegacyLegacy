c632eb735e0d8fd14aa8b19f451899fcdb725228
a <EYCHFSJD@gmail.com>
2025-02-27 14:36:04 +0800

    fix a silly bug

--- src/main/java/keletu/enigmaticlegacy/api/cap/EnigmaticCapabilities.java
+++ src/main/java/keletu/enigmaticlegacy/api/cap/EnigmaticCapabilities.java
@@ -11,10 +11,6 @@ public class EnigmaticCapabilities {
 
     @CapabilityInject(IPlaytimeCounter.class)
     public static final Capability<IPlaytimeCounter> PLAYTIME_COUNTER = null;
-    @CapabilityInject(IRelicsItemHandler.class)
-    public static Capability<IRelicsItemHandler> CAPABILITY_BAUBLES;
-    @CapabilityInject(IEnigmaticRelic.class)
-    public static Capability<IEnigmaticRelic> CAPABILITY_ITEM_RELIC;
 
     public static class CapabilityPlayerPlayTime implements IStorage<IPlaytimeCounter> {
 
@@ -31,31 +27,6 @@ public class EnigmaticCapabilities {
             NBTTagCompound tag = (NBTTagCompound) nbt;
             instance.setTimeWithCurses(tag.getLong("timeWithCurses"));
             instance.setTimeWithoutCurses(tag.getLong("timeWithoutCurses"));
-        }
-    }
-
-    public static class CapabilityRelics<T extends IRelicsItemHandler> implements IStorage<IRelicsItemHandler> {
-
-        @Override
-        public NBTBase writeNBT(Capability<IRelicsItemHandler> capability, IRelicsItemHandler instance, EnumFacing side) {
-            return null;
-        }
-
-        @Override
-        public void readNBT(Capability<IRelicsItemHandler> capability, IRelicsItemHandler instance, EnumFacing side, NBTBase nbt) {
-        }
-    }
-
-    public static class CapabilityItemRelicStorage implements IStorage<IEnigmaticRelic> {
-
-        @Override
-        public NBTBase writeNBT(Capability<IEnigmaticRelic> capability, IEnigmaticRelic instance, EnumFacing side) {
-            return null;
-        }
-
-        @Override
-        public void readNBT(Capability<IEnigmaticRelic> capability, IEnigmaticRelic instance, EnumFacing side, NBTBase nbt) {
-
         }
     }
 }
