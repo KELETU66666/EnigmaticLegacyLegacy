5972cae6b0fba215fb3c1ea84ea1471a63e6dcf2
a <EYCHFSJD@gmail.com>
2025-03-18 16:23:14 +0800

    fix a silly bug

--- src/main/java/keletu/enigmaticlegacy/packet/PacketSyncCapability.java
+++ src/main/java/keletu/enigmaticlegacy/packet/PacketSyncCapability.java
@@ -39,16 +39,19 @@ public class PacketSyncCapability implements IMessage {
         @Override
         public IMessage onMessage(final PacketSyncCapability message, MessageContext ctx) {
             if (ctx.side.isClient()) {
-                Minecraft.getMinecraft().addScheduledTask(() -> {
-                    EntityPlayer player = Minecraft.getMinecraft().player;
-                    if (player != null) {
-                        IPlaytimeCounter capability = player.getCapability(EnigmaticCapabilities.PLAYTIME_COUNTER, null);
-                        if (capability != null && message.data != null && message.type == 1) {
-                            capability.deserializeNBT(message.data);
-                        }
-                        IForbiddenConsumed capability1 = player.getCapability(EnigmaticCapabilities.FORBIDDEN_FRUIT_CONSUME, null);
-                        if (capability1 != null && message.data != null && message.type == 2) {
-                            capability1.deserializeNBT(message.data);
+                Minecraft.getMinecraft().addScheduledTask(new Runnable() {
+                    @Override
+                    public void run() {
+                        EntityPlayer player = Minecraft.getMinecraft().player;
+                        if (player != null) {
+                            IPlaytimeCounter capability = player.getCapability(EnigmaticCapabilities.PLAYTIME_COUNTER, null);
+                            if (capability != null && message.data != null && message.type == 1) {
+                                capability.deserializeNBT(message.data);
+                            }
+                            IForbiddenConsumed capability1 = player.getCapability(EnigmaticCapabilities.FORBIDDEN_FRUIT_CONSUME, null);
+                            if (capability1 != null && message.data != null && message.type == 2) {
+                                capability1.deserializeNBT(message.data);
+                            }
                         }
                     }
                 });
