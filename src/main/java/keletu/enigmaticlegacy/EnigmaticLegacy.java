3fabd9c780d7b3fc71f0c3d3d7927306ec359df1
a <EYCHFSJD@gmail.com>
2025-07-22 18:10:46 +0800

    change version name.

--- src/main/java/keletu/enigmaticlegacy/EnigmaticLegacy.java
+++ src/main/java/keletu/enigmaticlegacy/EnigmaticLegacy.java
@@ -71,7 +71,7 @@ public class EnigmaticLegacy {
 
     public static final String MODID = "enigmaticlegacy";
     public static final String MOD_NAME = "Enigmatic Legacy²";
-    public static final String VERSION = "2.0.0-rlc";
+    public static final String VERSION = "2.0.0";
 
     @SidedProxy(clientSide = "keletu.enigmaticlegacy.proxy.ClientProxy", serverSide = "keletu.enigmaticlegacy.proxy.CommonProxy")
     public static CommonProxy proxy;
